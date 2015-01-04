4ece62b23aa8fe4048150ca6ddea6d470bbe5b2e
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-01-04 21:39:22 -0200

    Added 3 more exercises and shortened the deadline!

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Solutions of the exercices rails the hard way -> http://learnrubythehardway.org/book/index.html
-Stated 01-01-2015 
-Hope to finish 14-02-2015 =)
+My Solutions of the exercices rails the hard way -> http://learnrubythehardway.org/book/index.html
+Stated 01-01-2015
+Hope to finish 31-01-2015 =)
---
+++ ex5-strings.rb
@@ -0,0 +1,16 @@
+my_name = "Barbara Barbosa C. da SIlva"
+my_age = 23
+my_height = 53
+my_weight = 163
+my_eyes = "brown"
+my_teeth = "white"
+my_hair = "red"
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} inches tall"
+puts "He's #{my_weight} pounds heavy"
+puts "Actually that's not too heavy"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth usually #{my_teeth} depending on the coffee"
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}. "
